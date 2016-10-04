799d84021f9bbf2676d76acd975a3f93d4b9189c
Nickolay <nickolay02@inbox.ru>
2016-10-04 21:23:58 +0300

    Trying to make it run on heroku

--- Rakefile
+++ Rakefile
@@ -3,3 +3,6 @@ task default: %w[run]
 task :run do
   ruby "src/main.rb"
 end
+task :rundev do
+  sh "ruby src/main.rb -p 8080"
+end
--- src/main.rb
+++ src/main.rb
@@ -4,7 +4,6 @@ require "sass"
 ENV["PORT"] = "8080"
 
 set :public_folder, File.dirname(__FILE__) + '../public'
-set :port, 8080
 set :bind, "0.0.0.0"
 pName = "Colorizer"
 $rColor = Sass::Script::Value::Color.from_hex("#ffffff")
